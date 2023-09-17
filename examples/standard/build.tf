6f2924bf1d3cb1c9385f934c8b7724e785abade6
Craig Thacker <craigthackerx@gmail.com>
2023-09-17 20:12:58 +0100

    Update module

--- examples/standard/build.tf
+++ examples/standard/build.tf
@@ -1,5 +1,50 @@
-module "test" {
+module "rg" {
+  source = "cyber-scot/rg/azurerm"
+
+  name     = "rg-${var.short}-${var.loc}-${var.env}-01"
+  location = local.location
+  tags     = local.tags
+}
+
+module "network" {
+  source = "cyber-scot/network/azurerm"
+
+  rg_name  = module.rg.rg_name
+  location = module.rg.rg_location
+  tags     = module.rg.rg_tags
+
+  vnet_name          = "vnet-${var.short}-${var.loc}-${var.env}-01"
+  vnet_location      = module.rg.rg_location
+  vnet_address_space = ["10.0.0.0/16"]
+
+  subnets = {
+    "sn1-${module.network.vnet_name}" = {
+      prefix            = "10.0.0.0/24",
+      service_endpoints = ["Microsoft.Storage"]
+    }
+  }
+}
+
+module "nsg" {
   source = "../../"
 
+  rg_name  = module.rg.rg_name
+  location = module.rg.rg_location
+  tags     = module.rg.rg_tags
 
+  nsg_name              = "nsg-${var.short}-${var.loc}-${var.env}-01"
+  associate_with_subnet = true
+  subnet_id             = element(values(module.network.subnets_ids), 0)
+  custom_nsg_rules = {
+    "AllowVnetInbound" = {
+      priority                   = 100
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "*"
+      source_address_prefix      = "VirtualNetwork"
+      destination_address_prefix = "VirtualNetwork"
+    }
+  }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,19 @@
-#
+output "nsg_id" {
+  value       = azurerm_network_security_group.nsg.id
+  description = "The ID of the NSG"
+}
+
+output "nsg_name" {
+  value       = azurerm_network_security_group.nsg.name
+  description = "The name of the NSG"
+}
+
+output "nsg_rg_name" {
+  value       = azurerm_network_security_group.nsg.resource_group_name
+  description = "The name of the resource group the nsg is in"
+}
+
+output "final_nsg_rules" {
+  value       = local.final_nsg_rules
+  description = "The NSG rules list assigned as a variable"
+}
